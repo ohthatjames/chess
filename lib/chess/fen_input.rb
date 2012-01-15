647391adb5f9533f606b8d208094c4732c1d6850
James Hunt <ohthatjames@googlemail.com>
2012-01-15 17:36:44 +0000

    Make #pieces_from_fen an instance method.
    This means I don't have to pass the state in as an argument. Not sure about
    the name. It's not really getting pieces, but the positions of the pieces.
    
    I think I may have gone far enough with refactoring before considering what
    happens if the FEN string is wrong. What happens if they give me a character
    I'm not expecting? Or if one of the rows has more or less than 8 entries?

--- lib/chess/fen_input.rb
+++ lib/chess/fen_input.rb
@@ -4,12 +4,13 @@ class FenInput
   end
 
   def squares
-    FenInput.pieces_from_fen(@fen).
+    pieces_from_fen.
       gsub(/\//, "\n").
       gsub(/(\d)/) {|m| "." * $1.to_i}
   end
 
-  def self.pieces_from_fen(fen)
-    fen.split(" ").first
+  private
+  def pieces_from_fen
+    @fen.split(" ").first
   end
 end

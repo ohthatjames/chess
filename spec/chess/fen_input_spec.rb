2f7022fd55d6b6132cbdff41a70d3485a3b40906
James Hunt <ohthatjames@googlemail.com>
2012-01-15 17:43:43 +0000

    Update FenInput#squares example name

--- spec/chess/fen_input_spec.rb
+++ spec/chess/fen_input_spec.rb
@@ -2,7 +2,7 @@ require 'chess/fen_input'
 require 'support/position_matcher'
 
 describe FenInput do
-  describe ".squares(fen)" do
+  describe "#squares" do
     it "returns the positions of all the pieces on the board" do
       squares = FenInput.new("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2").squares
       squares.should match_position <<-EOF
